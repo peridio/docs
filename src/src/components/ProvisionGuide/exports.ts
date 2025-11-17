d71fba343d38ba19278a6364bf8ce20439c20ca6
Lee Reinhardt <l.reinhardt@peridio.com>
2025-11-17 09:39:06 -0700

    fix misc type errors

--- src/src/components/ProvisionGuide/exports.ts
+++ src/src/components/ProvisionGuide/exports.ts
@@ -1,4 +1,2 @@
 export { default as ProvisionGuide } from './index'
 export type { ProvisionGuideProps } from './index'
-export { default as ProvisionContent, useProvisionContent } from './ProvisionContent'
-export type { ProvisionContentProps, ProvisionSections } from './ProvisionContent'
