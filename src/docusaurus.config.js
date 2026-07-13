a3d4eae02a48043f5a5d19e0abb63c81f98511e5
Cesar <c.rengifo@peridio.com>
2026-07-13 15:48:41 -0600

    Redirect the retired Download page to the Desktop overview

--- src/docusaurus.config.js
+++ src/docusaurus.config.js
@@ -61,6 +61,11 @@ const config = {
           // Avocado Connect is the successor to Peridio Core; keep old bookmarks alive.
           { from: '/peridio-core/overview', to: '/avocado-connect/overview' },
           { from: '/peridio-core', to: '/avocado-connect/overview' },
+          // The Download & Install page was retired in favor of the Desktop overview.
+          {
+            from: '/developer-reference/getting-started/download',
+            to: '/avocado-desktop/overview',
+          },
         ],
       },
     ],
