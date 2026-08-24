636b2bdce1e4b91b9cd89dc545ce05c789ae339c
Nick Sinas <nicksinas@gmail.com>
2026-08-24 14:10:04 -0500

    Add OTA guide to Developer Reference (ENG-2152) (#489)

--- src/sidebars-guides.js
+++ src/sidebars-guides.js
@@ -32,6 +32,7 @@ const sidebars = {
         'provisioning',
         'hardware-in-the-loop',
         'sideloading',
+        'ota',
         'package-feeds',
         'feed-search',
         'linux-auto-mounting',
